46772d91af00c7f0963abf9c7b66ead7aba96b8a
Nick Yannuzzi <nick.yannuzzi@credera.com>
2021-11-04 09:59:29 -0500

    begin react proj for mtg app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>
+            MTG Booster Sim
+          </p>
+          <p>
+            This is a fun tool made in React to simulate opening a pack of MTG cards based on set.
+          </p>
       </header>
+      
+      
     </div>
   );
 }
---
+++ src/SetDropdown.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class SetDropdown extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = 
+        {
+            sets : []
+        }
+    }
+    componentDidMount()
+    {
+        //get sets from mtg api (sdk), add to state
+    }
+}
